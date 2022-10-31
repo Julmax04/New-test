086c994794088789a3955b8fa401cd42c4dfac1a
Julmax <julianperez620@gmail.com>
2022-10-31 14:59:07 -0300

    Nuevos-archivos

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //tipos de variables: var, let, const
+// "var" tiene alcane global de la variable y "let" solo una paerte espesífica de la variable
     // tipos de datos :
 //String, boolean, number
+
+//Construccion de variables
+
+var Personaje1 = "kirito";
+ var edad = 16;
+ var soltero = true;
+
+ //datos especiales : Undefained, null, NAN
+
+ const nombre = "papa";
+ //las variables const solo pueden asignarse una vez y no pueden modificarse
+
+
+
 
 
