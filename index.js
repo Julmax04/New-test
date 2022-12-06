0478a9a311ce73f1477035fdcd59ae45c3b2cbb5
julmax <julianperez620@gmail.com>
2022-12-06 11:21:09 -0300

    practica

--- index.js
+++ index.js
@@ -1,1 +1,101 @@
+//Practica
 
+//Tipos de datos: string, boolean, number.
+
+//Lenguajes compilados y interpretaods
+
+
+//lenguaje imperativo: Se ejecuta linea por linea, case sensitivity: sensibilidad a las mayusculas y minusculas
+
+// var x = "hola"; //o
+
+//tipado debil
+
+// var x = "mama";
+
+//respetar los: ";" al finalizar cada linea de código
+
+
+//es.nex: "EsmaScriptNex" significa proxima actualización
+
+
+// var m = "Bienvenido ";
+
+// let l =  m + " mi señor";
+
+//String:  cadenas de texto
+
+//Boleans: numeros binarios 1 y 0 interpretado en true o flase
+//number: cadenas de numeros
+
+// Datos especiales: null, undefined, nan;
+
+//variables var, let y const
+
+
+
+//Undefined; es un dato que esta declarado pero no tiene valor
+
+var m = "Buenas ";
+let l = m + " Senior";
+
+
+
+// var tiene un alcance de escala global y local
+//let contiene un alcance limitado
+
+// const x = [];
+
+
+// Descripción. let te permite declarar variables limitando su alcance (scope) al bloque, declaración, o expresión donde se está usando. a diferencia de la palabra clave var la cual define una variable global o local en una función sin importar el ámbito del bloque.
+
+
+
+function open (){
+
+  alert(l)
+};
+
+
+// open();
+
+var x = null;
+
+//Prompt: es un sitema de alerta para poder escribir en el campo
+
+//NaN: NOT a Number: no se pueden hacer operaciones si no es un numero
+
+//para guardar las variables en un sistema de alerta, es con esta sintaxis
+
+//let number = prompt(" ");
+
+//ej:
+
+// let reg = prompt("Escribe tu nombre");
+
+// var text = "Bienvenido ";
+
+// function saludo(){
+
+//     alert( text + reg )  
+// };
+
+// saludo();
+
+
+
+//operadores de asignacion y ariméticos
+
+
+//el document.write asigna los valores que ubiquemos entre los parentesis imprimiendolos en el cuerpo del html 
+
+
+//Opereadores matematicos : +,-,*,/,%
+
+//Asignaciones: +=, -=, /=, %=, (Exponencial)**=; (Increment) ++ ; (Decrement) --, subtracción: -, 
+
+let prueba = 10;
+
+
+
+document.write(prueba);
