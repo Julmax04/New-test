44caf7b76cd38272ecc69a920938f64364462616
julmax <julianperez620@gmail.com>
2023-01-09 10:40:38 -0300

    Mod-2

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-let text = prompt ("Escribe Primer number ");
-let text2 = prompt ("Escribe Segundo number ");
-let text3 = prompt ("Escribe Trecer number ");
 
 
-function mayorOMenor (){
-    if(text >= 10){
 
-        alert(text);
 
-    } else if (text2 < 9) {
 
-         alert(text2);
-
-    } else if(text3 < 9){
-
-        alert(text3);
-
-    } else{
-        
-        alert("Invalido!")
-    }
-}
-
-mayorOMenor();
